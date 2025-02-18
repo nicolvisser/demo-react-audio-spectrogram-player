55360aa2360db809b25975204fcfc6d7d255a5c1
nicolvisser <nicolvisser@yahoo.com>
2025-02-18 16:30:09 +0200

    demo your own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,44 @@
-import './App.css'
 import SpectrogramPlayer from "react-audio-spectrogram-player";
+import "./App.css";
+import { useState } from "react";
 
-import sxx from "./assets/librispeech/sxx/19-198-0001.json"
-import src from "./assets/librispeech/wav/19-198-0001.wav"
-import wordIntervals from "./assets/librispeech/word_intervals/19-198-0001.json"
-import phoneIntervals from "./assets/librispeech/phone_intervals/19-198-0001.json"
+import phoneIntervals from "./assets/librispeech/phone_intervals/19-198-0001.json";
+import src from "./assets/librispeech/wav/19-198-0001.wav";
+import wordIntervals from "./assets/librispeech/word_intervals/19-198-0001.json";
 
 function App() {
-
-  const annotations = [
+  const [audioSrc, setAudioSrc] = useState(src);
+  const [currentAnnotations, setCurrentAnnotations] = useState([
     {
       title: "Word Intervals",
-      data: wordIntervals
+      data: wordIntervals,
     },
     {
       title: "Phone Intervals",
-      data: phoneIntervals
+      data: phoneIntervals,
+    },
+  ]);
+
+  const handleAudioUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setAudioSrc(url);
+      setCurrentAnnotations([]); // Clear annotations for user-uploaded audio
     }
-  ]
-
-  /*
-
-      src: C,
-    sxx: B = void 0,
-    sampleRate: E = 16e3,
-    n_fft: n = 1024,
-    win_length: e = 400,
-    hop_length: i = 160,
-    f_min: d = 0,
-    f_max: s = E / 2,
-    n_mels: o = 128,
-    top_db: a = 80,
-    annotations: t = [],
-    navigator: D = !1,
-    settings: h = !1,
-    startTimeInitial: b = void 0,
-    endTimeInitial: S = void 0,
-    playbackSpeedInitial: y = 1,
-    playheadModeInitial: R = "page",
-    specHeight: l = 200,
-    navHeight: G = 50,
-    colormap: w = "viridis",
-    transparent: j = !1,
-    dark: Z = !1
-    */
+  };
 
   return (
     <div className="App">
       <h1>react-audio-spectrogram-player</h1>
+      <div className="audio-upload">
+        <input type="file" accept="audio/*" onChange={handleAudioUpload} />
+        <p>Upload your own audio file or use the demo below</p>
+      </div>
       <h2>Demo:</h2>
       <SpectrogramPlayer
-        src={src}
-        //sxx={sxx}
+        key={audioSrc}
+        src={audioSrc}
         sampleRate={16000}
         n_fft={1024}
         win_length={400}
@@ -60,11 +47,9 @@ function App() {
         f_max={8000}
         n_mels={128}
         top_db={80}
-        annotations={annotations}
+        annotations={currentAnnotations}
         navigator
         settings
-        startTimeInitial={0}
-        endTimeInitial={10}
         playbackSpeedInitial={1}
         playheadModeInitial="page"
         specHeight={250}
@@ -74,14 +59,19 @@ function App() {
         dark
       />
       <p>
-      Audio used from <a href="https://www.openslr.org/12">LibriSpeech</a> dataset.
+        Audio used from <a href="https://www.openslr.org/12">LibriSpeech</a>{" "}
+        dataset.
       </p>
       <h2>Links:</h2>
-      <a href='https://github.com/nicolvisser/react-audio-spectrogram-player'>GitHub</a>
+      <a href="https://github.com/nicolvisser/react-audio-spectrogram-player">
+        GitHub
+      </a>
       <br />
-      <a href='https://www.npmjs.com/package/react-audio-spectrogram-player'>NPM</a>
+      <a href="https://www.npmjs.com/package/react-audio-spectrogram-player">
+        NPM
+      </a>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
